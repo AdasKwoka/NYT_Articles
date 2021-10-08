17c9fbec6934d30f302f2e8bdb2010962ffaf624
Adam Kwoka <adam.kwoka99@gmail.com>
2021-10-08 07:43:15 +0200

    Change style for articles

--- js/Article.js
+++ js/Article.js
@@ -19,11 +19,12 @@ class Article {
     return `
       <article class='article d-flex d-column border-bottom flex-wrap justify-content-center' data-id="${this.id}">
         <div class='article-text-wrap container row'>
-          <div class='article-img-wrap overflow-hidden col-3 ps-0 pe-0 mt-2'>
+          <div class='article-img-wrap overflow-hidden col-3 ps-0 pe-0 mt-2 mb-md-3'>
             <img class='mw-100 mh-100' src='${this.url_small}' alt='article-main-image'/>
           </div>
-          <a class='article-header fs-6 fw-bold text-decoration-none text-primary text-start text-wrap d-block ps-2 pe-0 col-9 mt-2 d-md-flex justify-content-md-center align-items-md-center' href='${this.url_website}' target='_blank'><span>${this.title}<span></a>  
-          <div class='article-text mt-sm-3 col-12 ps-0 pe-0 me-0 ms-0'>
+          <a class='article-header fs-6 fw-bold text-decoration-none text-primary text-start text-wrap d-block ps-2 pe-0 col-9 mt-2 d-md-flex justify-content-md-center align-items-md-center d-md-none' href='${this.url_website}' target='_blank'><span>${this.title}<span></a>  
+          <div class='article-text mt-sm-3 col-12 ps-0 pe-0 me-0 ms-0 ps-md-2 col-md-9'>
+            <a class='article-header fs-6 fw-bold text-decoration-none text-primary text-start text-wrap d-none pe-0 col-9 d-md-block' href='${this.url_website}' target='_blank'><span>${this.title}<span></a>
             <p class='article-description mb-1'>${this.lead_paragraph}</p>
             <p class='article-date mb-1 text-end'>${this.createDate()}</p>
             <button type="button" class="article-btn article-add-info btn btn-primary btn-sm mx-auto d-block mb-2 mb-sm-4">Show info</button>
